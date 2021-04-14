b0a676bb81eca491b5a66e91794b9cf46ba36d83
shamimbinnur <sbnsbn00@gmail.com>
2021-04-14 18:20:35 +0600

    added hadith and iftar time cards

--- src/user/components/IftarTime.js
+++ src/user/components/IftarTime.js
@@ -1,9 +1,42 @@
 import React from 'react'
-
+import { Card, CardContent, CardHeader, Typography, Grid, Container, makeStyles,
+    Paper,
+    Table,
+    TableContainer,
+    TableRow,
+    TableHead,
+    TableBody,
+    TableCell,
+} from '@material-ui/core'
 const IftarTime = ()=> {
+    const arr = [1,1,1,1,1,,1,1];
     return (
         <div>
-            IftarTime
+            <Container maxWidth='sm'>
+                <TableContainer component={Paper}>
+                    <Table  aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+ 
+                                <TableCell align="left"> <Typography> Division </Typography> </TableCell>
+                                <TableCell align="left"> <Typography> Time </Typography> </TableCell>
+
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                arr.map(item =>(
+                                    <TableRow>
+                                    <TableCell align="left"> <Typography> Dhaka </Typography> </TableCell>
+                                    <TableCell align="left"> <Typography> 6.30 PM </Typography> </TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            
+            </Container>
         </div>
     )
 }
--- src/user/components/LeaderBoard.js
+++ src/user/components/LeaderBoard.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import { Card, CardContent, CardHeader, Typography, Grid, Container, makeStyles,
+    Paper,
+    Table,
+    TableContainer,
+    TableRow,
+    TableHead,
+    TableBody,
+    TableCell,
+} from '@material-ui/core'
+import { WrapText } from '@material-ui/icons';
+import { blue } from '@material-ui/core/colors';
+const useStyles = makeStyles((theme)=>({
+    root:{
 
+    }
+   
+}))
 const LeaderBoard = ()=> {
+    const classes = useStyles();
+    const arr = [1,1,1,1,1,,1,1];
     return (
         <div>
-            Leader Board
+            <Container className={classes.root}  maxWidth='sm'>
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+ 
+                                <TableCell align="left"> <Typography> Serial </Typography> </TableCell>
+                                <TableCell align="left"> <Typography> Name </Typography> </TableCell>
+                                <TableCell align="left"> <Typography> City </Typography> </TableCell>
+                                <TableCell align="left"> <Typography> Score </Typography> </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                arr.map(item =>(
+                                    <TableRow>
+                                    <TableCell align="left"> <Typography> 01 </Typography> </TableCell>
+                                    <TableCell align="left"> <Typography> Shamim Bin Nur </Typography> </TableCell>
+                                    <TableCell align="left"> <Typography> Sirajganj </Typography> </TableCell>
+                                    <TableCell align="left"> <Typography> 405 </Typography> </TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            
+            </Container>
         </div>
     )
 }
